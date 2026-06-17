79b123a75d669802576c2968f68399d94faf6ece
richardjakasovic <richardjakasovic@gmail.com>
2026-06-17 23:20:28 +0200

    added breakers and part of logic

---
+++ Assets/Scripts/Interactables/Breaker.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Breaker : MonoBehaviour
+{
+    public bool isOn;
+    public List<Breaker> linkedBreakers;
+}
--- Assets/Scripts/Interactables/BreakerPanelButtonSpawner.cs
+++ Assets/Scripts/Interactables/BreakerPanelButtonSpawner.cs
@@ -1,24 +1,52 @@
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 
 public class BreakerPanelButtonSpawner : MonoBehaviour
 {
-    [SerializeField] private GameObject breakerButtonPrefab;
-    public List<bool> breakerList = new List<bool>();
+    [SerializeField] private GameObject breakerPrefab;
+    [SerializeField] private GameObject breakerStatusText;
     private SortedSet<string> breakerChars = new SortedSet<string> { "A", "B", "C", "D", "E", "F" };
+    public int difficulty = 4;
+    public List<Breaker> breakers = new List<Breaker>();
 
     void Start()
     {
         List<string> chars = new List<string>(breakerChars);
 
-        for (int i = 0; i < 4; i++)
+        for (int i = 0; i < difficulty; i++)
         {
-            GameObject newButton = Instantiate(breakerButtonPrefab, transform);
-            TextMeshProUGUI buttonText = newButton.GetComponentInChildren<TextMeshProUGUI>();
-
+            GameObject newBreaker = Instantiate(breakerPrefab, transform);
+            var breakerScript = newBreaker.GetComponent<Breaker>();
+            breakers.Add(breakerScript);
+            TextMeshProUGUI buttonText = newBreaker.GetComponentInChildren<TextMeshProUGUI>();
             buttonText.text = chars[i];
+            GameObject statusTextObject = Instantiate(breakerStatusText, transform);
+            TextMeshProUGUI statusText = statusTextObject.GetComponentInChildren<TextMeshProUGUI>();
 
+            
+            if (i == 0)
+            {
+                statusText.text = "ON";
+            }
+            if (i == 1)
+            {
+                statusText.text = "OFF";
+            }
+            if (i == 2)
+            {
+                statusText.text = "ON";
+            }
+            if (i == 3)
+            {
+                statusText.text = "ON";
+            }
         }
+    }
+
+    public bool isSolved()
+    {
+        return breakers.All(b => b.isOn);
     }
 }
